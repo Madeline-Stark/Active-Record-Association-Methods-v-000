be2f5d65530c5b160d90208ca6bca8be99d69007
Madeline Stark <madeline.stark1@gmail.com>
2018-08-21 17:00:28 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,8 +15,9 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    binding.pry
-    self.artists.name
+    self.artists.collect do |artist|
+      artist.name
+    end 
     # return an array of strings containing every musician's name
   end
 end
